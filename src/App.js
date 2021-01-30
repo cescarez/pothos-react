ba2c38d226b7bc820f28c906e6aeb9b0004101b6
Christabel Sebastian <christabel.escarez@gmail.com>
2021-01-29 17:17:28 -0800

    added rendering of returned json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
 import { Navbar, Nav, Button } from 'react-bootstrap';
@@ -7,24 +7,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [response, setResponse] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
   axios.get('http://localhost:5000/hello')
-  .then(res => res)
+  .then(res => {
+    console.log(res)
+    setResponse(res.data.message)
+  })
+  .catch((error) => {
+    const message=`There was an error with your request. ${error.message}.`;
+    setErrorMessage(message);
+    console.log(message);
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {response}
+
     </div>
   );
 }
