05042c6e7fa1781613522ed6ce2ccbc03db4bd65
Christabel Sebastian <christabel.escarez@gmail.com>
2021-02-02 09:20:30 -0800

    implemented logout in navigation bar instead of as a card on dashboard

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -19,39 +19,39 @@ const Navigation = () => {
         }
     }
 
-    const logoutForm = () => {
-        return (
-            <div>
-                <Card>
-                    <Card.Body>
-                        <h2 className='text-center mb-4'>Profile</h2>
-                        {error && <Alert variant='danger'>{error}</Alert>}
-                        <strong>Email:</strong> {currentUser.email}
-                        <Link to='/update-profile' className='btn btn-primary w-100 mt-3'>
-                            Update Profile
-                        </Link>
-                    </Card.Body>
-                </Card>
-                <div className='w-100 text-center mt-2'>
-                    <Button variant='link' onClick={handleLogout}>
-                        Log Out
-                    </Button>
-                </div>
-            </div>
-        )
-
-    }
-
+    // const logoutForm = () => {
+    //     return (
+    //         <div>
+    //             <Card>
+    //                 <Card.Body>
+    //                     <h2 className='text-center mb-4'>Profile</h2>
+    //                     {error && <Alert variant='danger'>{error}</Alert>}
+    //                     <strong>Email:</strong> {currentUser.email}
+    //                     <Link to='/update-profile' className='btn btn-primary w-100 mt-3'>
+    //                         Update Profile
+    //                     </Link>
+    //                 </Card.Body>
+    //             </Card>
+    //             <div className='w-100 text-center mt-2'>
+    //                 <Button variant='link' onClick={handleLogout}>
+    //                     Log Out
+    //                 </Button>
+    //             </div>
+    //         </div>
+    //     )
+    // }
 
     return (
-          <Navbar fixed='top' bg="plant-dark" variant="dark">
-            <Nav className="mr-auto">
-              <Nav.Link as={Link} to='/'>Home</Nav.Link>
-              <Nav.Link as={Link} to='/sitters'>Plant Sitters</Nav.Link>
-            </Nav>
-            {currentUser ? <Button variant="outline-info" as={Link} >Log Out</Button> : <Button variant="outline-info" as={Link} to='/signup'>Sign Up</Button>} 
-            {/* <Button variant="outline-info" as={Link} to='/signup'>Sign Up</Button> */}
-          </Navbar>
+        <div className='navigation'>
+            <Navbar fixed='top' bg="plant-dark" variant="dark">
+                <Nav className="mr-auto">
+                    <Nav.Link as={Link} to='/'>Home</Nav.Link>
+                    {/* <Nav.Link as={Link} to='/sitters'>Plant Sitters</Nav.Link> */}
+                </Nav>
+                {currentUser ? <Button variant="outline-light" as={Link} >Log Out</Button> : <Button variant="outline-light" as={Link} to='/signup'>Sign Up</Button>} 
+            </Navbar>
+            {error && <Alert variant='danger'>{error}</Alert>}
+        </div>
     )
 }
 
