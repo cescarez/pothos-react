6d292e598bab7033ecd06359fcb31fb661679ec3
Christabel Sebastian <christabel.escarez@gmail.com>
2021-02-08 10:47:20 -0800

    hardcoded some addresses in sittermap to troubleshoot rendering timing -- markers get dropped before the geocode response has returned

--- src/components/SitterMap.js
+++ src/components/SitterMap.js
@@ -5,41 +5,76 @@ import axios from 'axios';
 
 const BASE_GEOCODE_URL = 'https://maps.googleapis.com/maps/api/geocode/json?address='
 
-const SitterMap = ({ sitterList, currentUserData }) => {
+// const SitterMap = ({ sitterList, currentUserData }) => {
+const SitterMap = () => {
     const [error, setError] = useState({});
     const [mapCenter, setMapCenter] = useState({ lat: 39.8097343, lng: -98.5556199 });
     const [zoom, setZoom] = useState(8);
+
+    ///TEST DATA FOR /sitter-map ENDPOINT
+    const currentUserData = {
+        full_name: 'Seattle Space Needle',
+        address: {
+            street: '400 Broad St.',
+            city: 'Seattle',
+            state: 'WA',
+            postal_code: '98109'
+        }
+    }
+
+    const sitterList = [
+        {full_name: 'Seattle Childrens Theatre',
+         address: {
+            street: '201 Thomas St.',
+            city: 'Seattle',
+            state: 'WA',
+            postal_code: '98109'
+        }},
+        {full_name: 'Seattle Pacific Science Center',
+         address: {
+            street: '200 2nd Ave N.',
+            city: 'Seattle',
+            state: 'WA',
+            postal_code: '98109'
+        }}
+    ]
 
 
     useEffect(() => {
         currentUserData &&
             axios.get(createGeocodeURL(currentUserData))
                 .then((response) => {
-                    setMapCenter(response.data.results[0].geometry.location);
                     setZoom(11);
+                    setMapCenter(response.data.results[0].geometry.location);
                 })
                 .catch((error) => {
                     const message = `Did not load current user data. ${error.message}`
                     setError({ variant: 'warning', message: message })
                 })
-    }, [currentUserData])
+    }, [])
+
+    const loadSitterListMarkers = () => {
+        sitterList.forEach((sitter) => {
+            setTimeout(()=>{
+                axios.get(createGeocodeURL(sitter))
+                    .then((response) =>{
+                        sitter.address_coords = response.data.results[0].geometry.location
+
+                        console.log(`successfully set user address coords for user ${sitter.full_name}`)
+                    })
+                    .catch((error) => {
+                        const message = `Did not load sitter ${sitter.full_name} user data. ${error.message}`
+                        setError({ variant: 'warning', message: message })
+                        console.log(message);
+                    })
+            }, 3000)
+        })
+    }
 
     useEffect(() => {
         sitterList &&
-            sitterList.forEach((sitter) => {
-                setTimeout(()=>{
-                    axios.get(createGeocodeURL(sitter))
-                        .then((response) =>{
-                            sitter.address_coords = response.data.results[0].geometry.location
-                        })
-                        .catch((error) => {
-                            const message = `Did not load sitter ${sitter.full_name} user data. ${error.message}`
-                            setError({ variant: 'warning', message: message })
-                        })
-                }, 3000)
-        })
-    }, [sitterList])
-
+            loadSitterListMarkers();
+    }, [])
 
     const createGeocodeURL = (user) => {
         return (
@@ -50,31 +85,32 @@ const SitterMap = ({ sitterList, currentUserData }) => {
         )
     }
 
-    const currentUserMarker = () => {
+    const showCurrentUserMarker = () => {
         return (
             <Marker position={mapCenter} label='You' />
         )
     }
 
-    const sitterMarkers = () => {
-        return(
-            sitterList.forEach((sitter)=>{
+    const showSitterMarkers = () => {
+        sitterList.forEach((sitter)=>{
+            console.log(`dropped marker for ${sitter.full_name}`);
+            return(
                 <Marker
                     position={sitter.address_coords}
                     label={sitter.full_name}
-                    icon='http://maps.google.com/mapfiles/ms/icons/blue.png'
+                    // icon='http://maps.google.com/mapfiles/ms/icons/blue.png'
                 />
-            })
-        )
+            )
+        })
     }
-
+    
     const SitterMap = withScriptjs(withGoogleMap(((props) =>
         <GoogleMap
             defaultZoom={zoom}
             defaultCenter={mapCenter}
         >
-            {currentUserData && currentUserMarker()}
-            {sitterList && sitterMarkers()}
+            {currentUserData && showCurrentUserMarker()}
+            {sitterList && showSitterMarkers()}
 
         </GoogleMap>
     )))
