e6b97af7177aadc7b07ac4bd96969a75002358d9
Jessica Chan <jwinchan@gmail.com>
2021-02-11 21:33:02 -0800

    changed button styling in chatlog and gallery

--- src/components/Stripe.js
+++ src/components/Stripe.js
@@ -4,7 +4,7 @@ import { loadStripe } from "@stripe/stripe-js";
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
-const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_API_KEY);
 const ProductDisplay = ({ handleClick }) => (
   <section>
     <div className="product">
