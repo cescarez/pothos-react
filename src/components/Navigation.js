59cf75e47a7de3076a4c4d169187c47e5fe20dc7
Christabel Sebastian <christabel.escarez@gmail.com>
2021-02-11 16:30:01 -0800

    made checkout cart an icon for symmetry

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,8 @@ import React, {useState} from 'react';
 import { Navbar, Nav, Button, Alert, Dropdown, Container, Row } from 'react-bootstrap';
 import { useAuth } from '../contexts/AuthContext';
 import {useHistory, Link} from 'react-router-dom';
-import { HiOutlineCog, HiOutlineChatAlt } from 'react-icons/hi';
+import { BiCog, BiCart } from 'react-icons/bi';
+import { HiOutlineChatAlt } from 'react-icons/hi';
 
 import './Navigation.css';
 
@@ -27,9 +28,10 @@ const Navigation = () => {
             <Navbar fixed='top' bg='primary' variant='dark' >
                 <Container className='d-inline-flex justify-content-start'>
                     { currentUser &&
+                        <>
                         <Dropdown navbar>
                             <Dropdown.Toggle variant='primary' className='cog-icon' id='account-settings'>
-                                <HiOutlineCog className='cog'/>
+                                <BiCog className='cog'/>
                             </Dropdown.Toggle>
                             <Dropdown.Menu>
                                 <Dropdown.Item href="/update-email">Update Email/Password</Dropdown.Item>
@@ -38,13 +40,12 @@ const Navigation = () => {
                                 <Dropdown.Item onClick={handleLogout}>Log Out</Dropdown.Item>
                             </Dropdown.Menu>
                         </Dropdown>
-                    }
-                    { currentUser &&
                         <Nav.Link as={Link} to='/inbox' className='py-0'>
                             <Button>
                                 <HiOutlineChatAlt className='chat-icon'/>
                             </Button>
                         </Nav.Link>
+                        </>
                     }
                 </Container>
                 <Container className='d-inline-flex justify-content-center'>
@@ -52,7 +53,9 @@ const Navigation = () => {
                 </Container>
                 <Container className='d-inline-flex justify-content-end'>
                     {currentUser &&
-                        <Button className='checkout-button' variant='secondary'>Check Out</Button>
+                        <Button className='checkout-button' variant='secondary'>
+                            <BiCart />
+                        </Button>
                     }
                     {!currentUser &&
                         <Button variant='secondary' as={Link} to='/signup'>Sign Up</Button>
