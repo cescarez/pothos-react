2c1d3ae83bed3760f5011063bf9a8ea45d510763
Jessica Chan <jwinchan@gmail.com>
2021-02-16 09:06:36 -0800

    remove comments

--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -70,14 +70,12 @@ const Inbox = ({ baseURL, maxRating }) => {
                 message: 'Payment confirmed!'
             });
         }
-    
         if (query.get('canceled')) {
             setError({
                 variant: 'warning',
                 message: 'Payment canceled.'
             });
         }
-    
     }, []);
 
     const handleClick = async (event) => {
@@ -88,15 +86,11 @@ const Inbox = ({ baseURL, maxRating }) => {
     
         const session = await response.json();
         // When the customer clicks on the button, redirect them to Checkout.
-    
         const result = await stripe.redirectToCheckout({
             sessionId: session.id,
         });
     
         if (result.error) {
-          // If `redirectToCheckout` fails due to a browser or network
-          // error, display the localized error message to your customer
-          // using `result.error.message`.
             setError({
                 variant: 'warning',
                 message: result.error.message
