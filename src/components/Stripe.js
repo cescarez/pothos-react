32a87255b621e089fb6f00c0755cd4707e26edda
Jessica Chan <jwinchan@gmail.com>
2021-02-16 14:26:00 -0800

    removed usestate and useeffect from stripe

--- src/components/Stripe.js
+++ src/components/Stripe.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { loadStripe } from "@stripe/stripe-js";
 import { Button } from 'react-bootstrap'
 import { FaStripeS } from 'react-icons/fa';
