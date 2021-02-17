7c46985035701c99d1bf3ed6d658c2749a7319a1
Jessica Chan <jwinchan@gmail.com>
2021-02-16 22:19:58 -0800

    contact page

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -2,8 +2,28 @@ import React from 'react';
 
 export default function ContactUs() {
     return(
-        <div>
-            <h2>Contact Us</h2>
+        <div className='align-center'>
+            <h2>Contact Us</h2> 
+            <br />
+            <div>
+                <h5>General Inquiries</h5>
+                <a href='mailto:pothos.app@gmail.com'>contact@pothos.com</a>
+            </div>
+            <br />
+            <div>
+                <h5>Marketing / Press</h5>
+                <a href='mailto:pothos.app@gmail.com'>marketing@pothos.com</a>
+            </div>
+            <br />
+            <div>
+                <h5>Careers</h5>
+                <a href='mailto:pothos.app@gmail.com'>careers@pothos.com</a>
+            </div>
+            <br />
+            <div>
+                <h5>Philanthropy</h5>
+                <a href='mailto:pothos.app@gmail.com'>development@pothos.com</a>
+            </div>
         </div>
     )
 }
